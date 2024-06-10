33402226421ee72a2859e505bc65639b97a29d2a
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-06-10 21:00:15 +0900

    [feat] make CrownDataEntity

---
+++ MHWCrownApp/CrownDataEntity.swift
@@ -0,0 +1,15 @@
+//
+//  CrownDataEntity.swift
+//  MHWCrownApp
+//
+//  Created by 村石 拓海 on 2024/06/10.
+//
+
+import Foundation
+
+struct CrownDataEntity {
+    var isLimitMaximum: Bool = false
+    var isMaximum: Bool = false
+    var isMinimum: Bool = false
+    var isLimitMinimum: Bool = false
+}
