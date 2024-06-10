e024c4ba8f6ce42df1b1d5c2d9ca5b7773ab0c41
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-06-10 20:31:44 +0900

    [feat] Entityを8つ追加

--- MHWCrownApp/MonsterCrownData.swift
+++ MHWCrownApp/MonsterCrownData.swift
@@ -11,8 +11,38 @@ public struct MonsterCrownData {
     var crownDataEntitys: [CrownDataEntity]
 
     init() {
-        let firstEntity = CrownDataEntity(isLimitMaximum: true, isMaximum: true, isMinimum: true, isLimitMinimum: true)
-        let secondEntity = CrownDataEntity(isMinimum: true, isLimitMinimum: true)
-        self.crownDataEntitys = [firstEntity, secondEntity]
+        let firstEntity = CrownDataEntity(
+            id: 1,
+            name: "ラージャン"
+        )
+        let secondEntity = CrownDataEntity(
+            id: 2,
+            name: "激昂ラージャン"
+        )
+        let thirdEntity = CrownDataEntity(
+            id: 3,
+            name: "リオレウス"
+        )
+        let fourEntity = CrownDataEntity(
+            id: 4,
+            name: "リオレウス亜種"
+        )
+        let fifthEntity = CrownDataEntity(
+            id: 5,
+            name: "リオレウス希少種"
+        )
+        let sixthEntity = CrownDataEntity(
+            id: 6,
+            name: "リオレイア"
+        )
+        let seventhEntity = CrownDataEntity(
+            id: 7,
+            name: "リオレイア亜種"
+        )
+        let eighthEntity = CrownDataEntity(
+            id: 8,
+            name: "リオレイア希少種"
+        )
+        self.crownDataEntitys = [firstEntity, secondEntity, thirdEntity, fourEntity, fifthEntity, sixthEntity, seventhEntity, eighthEntity]
     }
 }
